7278062520bb034a14d30cf01b2705100818c31c
Munteanu Victor <54933783+VictorMunteanu@users.noreply.github.com>
2021-09-12 12:48:40 +0300

    Add files via upload

--- hello + cafea/src/cafea/CoffeeMachine.java
+++ hello + cafea/src/cafea/CoffeeMachine.java
@@ -4,32 +4,47 @@ import java.util.*;
 
 public class CoffeeMachine {
     private Boolean pay(Integer banknoteNominal){
-        Scanner payScan = new Scanner(System.in);
-        banknoteNominal = payScan.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        banknoteNominal = scanner.nextInt();
         if(banknoteNominal == 5 || banknoteNominal == 10){
             return true;
         }
         return false;
     }
 
-    private String choose(Integer optionCh){
-        Scanner chooseScan = new Scanner(System.in);
-        optionCh = chooseScan.nextInt();
-        if(optionCh == 1){
+    private String choose(Integer option){
+        Scanner scanner1 = new Scanner(System.in);
+        option = scanner1.nextInt();
+        if(option == 1){
             return "Latte";
-        } else if(optionCh == 2){
+        } else if(option == 2){
             return "Cappuccino";
+        } else if(option == 3){
+            return "Americano";
+        } else if(option == 4){
+            return "Mocha";
         }
         return "Incorrect option";
     }
 
-    private String sugar(String optionSu){
-        Scanner sugarScan = new Scanner(System.in);
-        optionSu = sugarScan.nextLine();
-        if(optionSu == "y"){
-            return "With sugar";
-        } else if(optionSu == "n"){
-            return "No sugar";
+    private String sugarCh(Integer optionSu){
+        Scanner sugar = new Scanner(System.in);
+        optionSu = sugar.nextInt();
+        if(optionSu == 1){
+            return "with sugar";
+        } else if(optionSu == 2){
+            return "without sugar";
+        }
+        return "Incorrect option";
+    }
+
+    private String milkCh(Integer optionMi){
+        Scanner scanner3 = new Scanner(System.in);
+        optionMi = scanner3.nextInt();
+        if(optionMi == 1){
+            return "with milk";
+        } else if(optionMi == 2){
+            return "without milk";
         }
         return "Incorrect option";
     }
@@ -44,20 +59,24 @@ public class CoffeeMachine {
 
     public void prepare(){
         String coffeeType = "";
+        String sugar = "";
+        String milk = "";
         this.showInfo("Please insert banknote");
         if(this.pay(5)){
-            this.showInfo("Please chose coffee type");
-            this.showInfo(
-                    "You choosed: " + coffeeType
-            );
-            this.showInfo("Please chose sugar option");
-            this.showInfo(
-                    "You choosed: " + coffeeType
-            );
-            this.showInfo("Preparing...");
-            this.showInfo(
-                    "Done: " + this.done(coffeeType)
-            );
+            this.showInfo("What kind of coffee you like?");
+            this.showInfo("1.Latte; 2.Cappuccino; 3.Americano; 4.Mocha");
+            coffeeType = this.choose(1);
+
+            this.showInfo(coffeeType + " with sugar or not?");
+            this.showInfo("1.with sugar; 2.no sugar");
+            sugar = this.sugarCh(1);
+
+            this.showInfo(coffeeType + " with milk or not?");
+            this.showInfo("1.with milk; 2.no milk");
+            milk = this.milkCh(1);
+
+            this.showInfo("Preparing... Please wait.");
+            this.showInfo("Ready: " + this.done(coffeeType + " " + sugar + " & " + milk));
         } else {
             this.showInfo("Please insert correct banknote");
         }
